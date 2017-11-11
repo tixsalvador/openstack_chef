b8307b08418aa9831756d8a0646402b766439cd8
tixsalvador <tix.salvador@gmail.com>
2017-11-11 13:27:28 -0500

    commit

--- cookbooks/prerequisites/metadata.rb
+++ cookbooks/prerequisites/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tixsalvador@me.com'
 license 'All Rights Reserved'
 description 'Installs/Configures prerequisites'
 long_description 'Installs/Configures prerequisites'
-version '0.3.2'
+version '0.3.4'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
--- cookbooks/prerequisites/recipes/packages.rb
+++ cookbooks/prerequisites/recipes/packages.rb
@@ -3,3 +3,15 @@ package 'chrony' do
   action :install
 end
 
+ruby_block "edit chrony" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/chrony.conf")
+    file.insert_line_after_match('^.*?allow\s\d{1,3}\.\d{1,3}.*$', 'allow 10.0.0.0/24') 
+    file.write_file
+  end
+  not_if "cat /etc/chrony.conf | grep 10.0.0.0/24"
+end
+
+service 'chronyd' do
+  subscribes :restart, "ruby_block[edit chrony]"
+end
