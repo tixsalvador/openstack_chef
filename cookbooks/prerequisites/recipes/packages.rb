469119ae75bf3a7e3203f7c1c6bee485804aff92
tixsalvador <tix.salvador@gmail.com>
2017-11-11 22:20:51 -0500

    commit

--- cookbooks/prerequisites/recipes/packages.rb
+++ cookbooks/prerequisites/recipes/packages.rb
@@ -3,15 +3,17 @@ package 'chrony' do
   action :install
 end
 
-ruby_block "edit chrony" do
-  block do
-    file = Chef::Util::FileEdit.new("/etc/chrony.conf")
-    file.insert_line_after_match('^.*?allow\s\d{1,3}\.\d{1,3}.*$', 'allow 10.0.0.0/24') 
-    file.write_file
-  end
-  not_if "cat /etc/chrony.conf | grep 10.0.0.0/24"
-end
+if node['hostname'] == "controller"
+   ruby_block "edit chrony" do
+     block do
+       file = Chef::Util::FileEdit.new("/etc/chrony.conf")
+       file.insert_line_after_match('^.*?allow\s\d{1,3}\.\d{1,3}.*$', 'allow 10.0.0.0/24') 
+       file.write_file
+     end #block
+     not_if "cat /etc/chrony.conf | grep 10.0.0.0/24"
+   end # ruby_block
 
-service 'chronyd' do
-  subscribes :restart, "ruby_block[edit chrony]"
-end
+   service 'chronyd' do
+     subscribes :restart, "ruby_block[edit chrony]"
+   end #service chronyd
+end #if node
