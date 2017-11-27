e2964320ba31e932d13f55f1d655d073443583ef
tix <tixsalvador@me.com>
2017-11-27 22:20:46 +0000

    Added Begin/End line at  host and openstack.cnf file

--- cookbooks/prerequisites/recipes/default.rb
+++ cookbooks/prerequisites/recipes/default.rb
@@ -1,12 +1,14 @@
 ruby_block 'host_file' do
   block do
     file = Chef::Util::FileEdit.new('/etc/hosts')
+    file.insert_line_if_no_match('#BEGIN HOSTFILE', '#BEGIN HOSTFILE')
     file.insert_line_if_no_match('10.0.0.11   controller', '10.0.0.11   controller')
     file.insert_line_if_no_match('10.0.0.31   compute', '10.0.0.31   compute')
     file.insert_line_if_no_match('10.0.0.41   block', '10.0.0.41   block')
     file.insert_line_if_no_match('10.0.0.51   object1', '10.0.0.51   object1')
     file.insert_line_if_no_match('10.0.0.52   object2', '10.0.0.52   object2')
     file.insert_line_if_no_match('10.0.0.61   controlmanager', '10.0.0.61   controlmanager')
+    file.insert_line_if_no_match('#END HOSTFILE', '#END HOSTFILE')
     file.write_file
   end
 end
